25e9a4dee29435b0f29bea97b2832bf3de138459
Jerome Azah <kafjer@gmail.com>
2020-02-26 18:15:59 +0000

    solved the war map challenge

--- hackerrank.php
+++ hackerrank.php
@@ -20,7 +20,7 @@ class HackerRank
         return $sum;
     }
 
-    function birthdayCakeCandles($ar) {
+    public function birthdayCakeCandles($ar) {
         rsort($ar);
         $sum = 0;
         $i = 0;
@@ -29,7 +29,24 @@ class HackerRank
             $i +=1;
         }
         return $sum;
+    }
+// the warmap challenge
+    public function timeConversion($s) {
 
+        $timeArray  = explode(":", $s);
+
+        $period = substr($timeArray[2],2);
+        $hour = $timeArray[0];
+        if($period == "PM" && $hour != 12){
+            $hour += 12;
+        }elseif($period == "AM" && $hour == 12){
+            $hour = "00";
+        }
+
+
+        $time = $hour.":".$timeArray[1].":".substr($timeArray[2],0,2);
+
+        return $time;
 
     }
 }
